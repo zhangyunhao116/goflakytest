76040b0bdf1fd7d846c048077591170e75f9f6e5
zhangyunhao <zhangyunhao116@gmail.com>
2023-11-06 16:10:07 +0800

    init

---
+++ ci.bash
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+set -e
+
+WORK_DIR="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/workspace"
+COMPILE_OPTS="$*"
+
+git submodule init
+git submodule sync --recursive
+
+# Simple tests.
+declare -a repos=(
+    # {REPO_NAME} {EXTRA_COMPILE_OPTS}
+    "fastcache" ""
+    "fasthttp" "-race"
+    "gin"  "-race"
+    "gopkg" "-race"
+    "skipmap" "-race"
+    "skipset" "-race"
+    "umap" ""
+)
+
+for ((i=0; i<${#repos[@]}; i+=2)); do
+  REPO_NAME="${repos[i]}"
+  EXTRA_COMPILE_OPTS="${repos[i+1]}"
+
+  CMD="go test ${COMPILE_OPTS} ${EXTRA_COMPILE_OPTS} -failfast -count=1 ./..."
+  echo \#\#\#\#\# TEST ${REPO_NAME} \#\#\#\#\# ${CMD}
+  cd $WORK_DIR/$REPO_NAME && ${CMD} && echo $REPO_NAME PASSED!
+done
+
+# Complicated tests.
+
+# k8s.
+cd $WORK_DIR/kubernetes && env GOFLAGS="-count=1" make test && echo kubernetes PASSED!
+
+# golangci-lint.
+cd $WORK_DIR/golangci-lint && env GOFLAGS="-count=1" make test && echo golangci-lint PASSED!
---
+++ readme.md
@@ -0,0 +1,22 @@
+# Tests for Go SDK
+
+
+## RUN
+
+### Simple CI (with options)
+
+`bash ci.bash`
+
+`bash ci.bash -opt=beast`
+
+### Full CI (no options)
+
+`bash fullci.bash`
+
+## Add submoudle
+
+`git submodule add https://github.com/zhangyunhao116/skipmap.git workspace/skipmap`
+
+## Remove submodule
+
+`git rm -f workspace/foo`
